d8311f234dafee07084d2a6c9237eca4725c4790
shaozx <shaozx@d24f4a9c-349c-11de-be05-5f7a86268029>
2009-05-06 16:42:50 +0000

    Version 20090426

--- chsdir/install.sh
+++
@@ -1,9 +0,0 @@
-
-mkdir -p $HOME/bin
-cp chsdir chs_completion $HOME/bin/
-[ $? -ne 0 ] && echo "FAIL!" && exit 3
-echo '. $HOME/bin/chs_completion' >> $HOME/.bashrc
-echo 'PATH=$PATH:$HOME/bin' >>  $HOME/.bashrc
-
-echo "Install finished."
-echo "Open new terminal to use it."
