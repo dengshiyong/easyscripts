f5a1b939d8d7cf55ae663f4d5b0ab02e459b8551
shaozx <shaozx@d24f4a9c-349c-11de-be05-5f7a86268029>
2009-09-01 01:37:10 +0000

    增加多音字支持 增加模糊拼音支持

--- chsdir/install.sh
+++ chsdir/install.sh
@@ -6,6 +6,7 @@
 
 mkdir -p $HOME/bin
 cp chsdir chs_completion $HOME/bin/
+chmod +x $HOME/bin/chsdir
 
 if [ "$SHELL" != "/bin/bash" ]
 then
@@ -16,10 +17,11 @@ fi
 CONF=$HOME/.bashrc
 [ $? -ne 0 ] && echo "FAIL!" && exit 3
 
-echo '### chsdir start ###'       >> $CONF
-echo '. $HOME/bin/chs_completion' >> $CONF
-echo 'PATH=$PATH:$HOME/bin'       >> $CONF
-echo '### chsdir finish. ###'     >> $CONF
+echo '### chsdir start ###'          >> $CONF
+echo '. $HOME/bin/chs_completion'    >> $CONF
+echo 'PATH=$PATH:$HOME/bin'          >> $CONF
+echo "#export CHSDIR=\"{'n':'l'}\""  >> $CONF
+echo '### chsdir finish. ###'        >> $CONF
 
 echo "Install finished."
 echo "Open new terminal to use it."
